750a3704475a12d48f6c57a566cd1f557005e309
seperry83 <seperry83@gmail.com>
2021-11-03 13:38:51 -0700

    data app

--- apps/data_app/data_app.R
+++ apps/data_app/data_app.R
@@ -8,6 +8,10 @@
 #
 
 library(shiny)
+library(tidyverse)
+
+df_compiled <- read_csv('../../data/annual_averages/annual_data_compiled.csv')
+df_compiled <- pivot_longer(df_compiled, cols = -Year, names_to = 'variable', values_to = 'value')
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -15,35 +19,51 @@ ui <- fluidPage(
     # Application title
     titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+    # Sidebar with a slider input for number of bins
+    # sidebarLayout(
+    #     sidebarPanel(
+    #         sliderInput("bins",
+    #                     "Number of bins:",
+    #                     min = 1,
+    #                     max = 50,
+    #                     value = 30)
+    #     ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+
+
+    # Show a plot of the generated distribution
+    fluidRow(
+        column(
+            width = 12, # width of entire row
+            selectInput(inputId = 'variable',
+                        multiple = TRUE,
+                        choices = unique(df_compiled$variable),
+                        label = 'Variable'
+                        )
+        )
+    ),
+
+    fluidRow(
+        column(
+            width = 12,
+            plotOutput("distPlot", height = '700px')
         )
     )
 )
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    df_sub <- reactive({subset(df_compiled, variable %in% input$variable)})
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$distPlot <- renderPlot({
+        # create histogram of all variables
+        ggplot(df_sub(), aes(x = value)) +
+            geom_histogram() +
+            facet_wrap(~variable, scales = 'free')
     })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
