095a6a8380d6035f42adcf8121d7eee26627595c
Matt Jones <gitcode@magisa.org>
2021-11-04 16:01:42 -0700

    Add targets example config files and functions.

---
+++ R/model_functions.R
@@ -0,0 +1,9 @@
+#' Run our first model on the derived dataset
+run_model1 <- function(data_integrated) {
+  print("Model1 not yet implemented.")
+}
+
+#' Run our second model on the derived dataset
+run_model2 <- function(data_integrated) {
+  print("Model2 not yet implemented.")
+}
