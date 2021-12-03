1cabd49b1587cdf978bfdb0f0f340ac57b488342
Parsa S <parsa.saffarinia@gmail.com>
2021-12-03 15:02:49 -0800

    calculated region specific values for STN monthly and annual

--- data/fish_STN_monthly.R
+++ data/fish_STN_monthly.R
@@ -1,10 +1,12 @@
+##STN_monthly
+
 devtools::install_github("sbashevkin/LTMRdata")
 library(LTMRdata)
 library(tidyverse)
 library(lubridate)
 library(ggmap)
+source("functions/region_assigner.R")
 
-setwd("~/GitHub/swg-21-foodwebs")
 
 #From Sam's LTMRdata package
 
@@ -24,14 +26,14 @@ STN_Striped_Bass <-LTMRdata::fish(sources="STN", species="Morone saxatilis", siz
 str(STN_Delta_Smelt)
 #Limit the data to just April to October (this was for baystudy, we aren't limiting the date extent)
 STN_combined <- bind_rows(STN_Delta_Smelt,STN_Longfin_Smelt,STN_Threadfin_Shad,
-                               STN_American_Shad,STN_Pacific_Herring,
-                               STN_Northern_Anchovy,STN_Striped_Bass) %>%
+                          STN_American_Shad,STN_Pacific_Herring,
+                          STN_Northern_Anchovy,STN_Striped_Bass) %>%
   mutate(Month=month(Date)) #%>% filter(Month %in% c(4:10)) %>%
-  #and only between San Pablo Bay and the confluence
-  #https://nrm.dfg.ca.gov/FileHandler.ashx?DocumentID=185001&inline
- # mutate(Station=as.numeric(Station)) %>%
-  #filter(Station>300) %>%
-  #filter(Station<700|Station %in% c(736,837))
+#and only between San Pablo Bay and the confluence
+#https://nrm.dfg.ca.gov/FileHandler.ashx?DocumentID=185001&inline
+# mutate(Station=as.numeric(Station)) %>%
+#filter(Station>300) %>%
+#filter(Station<700|Station %in% c(736,837))
 
 unique(STN_combined$Station)
 
@@ -41,7 +43,7 @@ STNCoords<-STNCoords[complete.cases(STNCoords), ]
 
 #not sure what this is
 #STNCoords_studyregion<-STNCoords %>% filter(Station>300) %>%
- # filter(Station<700|Station %in% c(736,837))
+# filter(Station<700|Station %in% c(736,837))
 
 ###
 buffer <- 0.05
@@ -80,8 +82,8 @@ ggplot(STN_sample_size,aes(x=Year,y=as.factor(Station),fill=sample_size))+geom_t
 #We probably need to remove the sites below due to lack of repeated data
 `%notin%` <- Negate(`%in%`)
 STNCoords_studyregion<-STNCoords %>% filter(Station %notin%
-  c(797,796,795,723,722,721,719,716,713,36,35,336,335,334,329,328,
-    326,322,320,32,315,312,302,301,24,23,22,21,20 ))
+                                              c(797,796,795,723,722,721,719,716,713,36,35,336,335,334,329,328,
+                                                326,322,320,32,315,312,302,301,24,23,22,21,20 ))
 
 #And add regions from STN for summarizing annual values
 STNCoords_studyregion<-STNCoords_studyregion %>%
@@ -109,14 +111,23 @@ str(STNCoords_studyregion)
 STNCoords_studyregion$Station<-as.character(STNCoords_studyregion$Station)
 STN_combined_derived$Station<-as.character(STN_combined_derived$Station)
 STN_combined_derived<-left_join(STN_combined_derived,STNCoords_studyregion)
-#Calculate final annual values by:
+
+##Sam's region assigner functiong gets pissed if we already have a region column
+##so i'm deleting previous region column we used
+STN_combined_derived <- subset(STN_combined_derived, select = -c(Region))
+#sam's region assigner assigns new region to samples
+STN_combined_derived<-STN_combined_derived%>%
+  region_assigner(analysis="monthly")
+
+#Calculate final monthly values by:
 #1) Calculate biomass based on Kimmerer et al. 2005
 #2) Averaging CPUE and biomass by region for each year
 #3) Average across region for each year to get an annual value
 
-STN_combined <- STN_combined %>%
+
+STN_combined_derived <- STN_combined_derived %>%
   mutate(Length=ifelse(is.na(Length),0,Length)) %>%
-  mutate(Year=year(Date)) %>%
+  mutate(Month=month(Date)) %>%
   mutate(Biomass = case_when(
     Taxa=="Hypomesus transpacificus" ~ (0.0018*(Length^3.38))*Count,
     Taxa=="Spirinchus thaleichthys" ~ (0.0005*(Length^3.69))*Count,
@@ -126,12 +137,12 @@ STN_combined <- STN_combined %>%
     Taxa=="Clupea pallasii" ~ (0.0015*(Length^3.44))*Count,
     Taxa=="Morone saxatilis" ~ (0.0066*(Length^3.12))*Count))
 
+##add separating by region
 
-
-STN_annual_values <- STN_combined_derived %>%
-  group_by(Year,Region,Method,Taxa) %>%
+STN_monthly_values <- STN_combined_derived %>%
+  group_by(Month,Region,Method,Taxa) %>%
   summarise(Biomass=mean(Biomass),Catch_per_tow=mean(Count)) %>%
-  group_by(Year,Method,Taxa) %>%
+  group_by(Month,Method,Taxa, Region) %>%
   summarise(Biomass=mean(Biomass),Catch_per_tow=mean(Catch_per_tow)) %>%
   mutate(CommonName= case_when(
     Taxa=="Hypomesus transpacificus" ~ "DeltaSmelt",
@@ -145,36 +156,36 @@ STN_annual_values <- STN_combined_derived %>%
 #If you check the original dataset, there are no unmeasured fish flag
 #Summarize biomass as you would normally
 
-
-STN_annual_values_CPUE <- STN_annual_values  %>% filter(Method=="STN Net") %>% subset(select=c(Year,CommonName,Catch_per_tow)) %>%
+##added "region" in the subset to have annual/regional values
+STN_monthly_values_CPUE <- STN_monthly_values  %>% filter(Method=="STN Net") %>% subset(select=c(Month,CommonName,Catch_per_tow, Region)) %>%
   pivot_wider(names_from =CommonName,values_from = Catch_per_tow,names_prefix="STN_fish_catch_per_tow_") %>%
   mutate(STN_fish_biomass_Estuarine_pelagic_forage_fishes=sum(STN_fish_catch_per_tow_AmericanShad,
-                                    STN_fish_catch_per_tow_ThreadfinShad,
-                                    STN_fish_catch_per_tow_DeltaSmelt,
-                                    STN_fish_catch_per_tow_LongfinSmelt,
-                                    STN_fish_catch_per_tow_StripedBass_age0),
-       STN_fish_catch_per_tow_Marine_pelagic_forage_fishes=sum(STN_fish_catch_per_tow_NorthernAnchovy,
-                                                                                    STN_fish_catch_per_tow_PacificHerring))
+                                                              STN_fish_catch_per_tow_ThreadfinShad,
+                                                              STN_fish_catch_per_tow_DeltaSmelt,
+                                                              STN_fish_catch_per_tow_LongfinSmelt,
+                                                              STN_fish_catch_per_tow_StripedBass_age0),
+         STN_fish_catch_per_tow_Marine_pelagic_forage_fishes=sum(STN_fish_catch_per_tow_NorthernAnchovy,
+                                                                 STN_fish_catch_per_tow_PacificHerring))
 
 
 
 
 
 
-STN_values_biomass <- STN_annual_values %>% filter(Method=="STN Net") %>% subset(select=c(Year,CommonName,Biomass)) %>%
+STN_values_biomass <- STN_monthly_values %>% filter(Method=="STN Net") %>% subset(select=c(Month,CommonName,Biomass, Region)) %>%
   pivot_wider(names_from =CommonName,values_from = Biomass,names_prefix="STN_fish_biomass_") %>%
   mutate(STN_fish_biomass_Estuarine_pelagic_forage_fishes=sum(STN_fish_biomass_AmericanShad,
-                                                                            STN_fish_biomass_ThreadfinShad,
-                                                                            STN_fish_biomass_DeltaSmelt,
-                                                                            STN_fish_biomass_LongfinSmelt,
-                                                                            STN_fish_biomass_StripedBass_age0),
-        STN_fish_biomass_Marine_pelagic_forage_fishes=sum(STN_fish_biomass_NorthernAnchovy,
-                                                                              STN_fish_biomass_PacificHerring))
+                                                              STN_fish_biomass_ThreadfinShad,
+                                                              STN_fish_biomass_DeltaSmelt,
+                                                              STN_fish_biomass_LongfinSmelt,
+                                                              STN_fish_biomass_StripedBass_age0),
+         STN_fish_biomass_Marine_pelagic_forage_fishes=sum(STN_fish_biomass_NorthernAnchovy,
+                                                           STN_fish_biomass_PacificHerring))
 
 
 
-STN_annual_values_final<-full_join(STN_annual_values_CPUE,
-                                            STN_values_biomass)
+STN_monthly_values_final<-full_join(STN_monthly_values_CPUE,
+                                   STN_values_biomass)
 
 
-write.csv(STN_annual_values_final,row.names=FALSE,file=file.path("data/annual_averages/fish_data_STN.csv"))
+write.csv(STN_monthly_values_final,row.names=FALSE,file=file.path("data/monthly_averages/fish_data_STN_monthly.csv"))
