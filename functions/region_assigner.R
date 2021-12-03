d27d20231ab5cf4db6693730af4272fe4e157f06
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2021-12-03 10:13:57 -0800

    Finishing region assignments and creating a function to assign regions and filter dataset by the regional extent

---
+++ functions/region_assigner.R
@@ -0,0 +1,43 @@
+region_assigner<-function(data, analysis, plot=TRUE){
+
+  require(sf)
+  require(dplyr)
+
+if(!analysis%in%c("monthly", "annual")){
+  stop("analysis must be one of either 'monthly' or 'annual'")
+}
+
+  if(!is.logical(plot)){
+    stop("plot must be a logical, either TRUE or FALSE")
+  }
+
+  if(!all(c("Latitude", "Longitude")%in%names(data))){
+    stop("data must contain columns named 'Latitude' and 'Longitude'")
+  }
+
+  regions<-readRDS("data/data_in/regions.Rds")
+
+data_regions<-data%>%
+  st_as_sf(coords=c("Longitude", "Latitude"), crs=4326, remove=FALSE)%>%
+  st_join(regions)%>%
+  st_drop_geometry()%>%
+  {if(analysis=="annual"){
+    mutate(., Region=if_else(Region=="Far West", NA_character_, Region))
+  }else{
+    .
+  }}
+
+if(plot){
+  require(ggplot2)
+  p<-ggplot()+
+    geom_sf(data=regions, aes(fill=Region))+
+    geom_point(data=data_regions%>%select(Longitude, Latitude, Region)%>%distinct(), aes(x=Longitude, y=Latitude, color=is.na(Region)))+
+    scale_color_manual(breaks=c(TRUE, FALSE), values=c("black", "white"), name="Is station outside the regions?")
+  print(p)
+}
+
+out<-data_regions%>%
+  filter(!is.na(Region))
+
+return(out)
+}
